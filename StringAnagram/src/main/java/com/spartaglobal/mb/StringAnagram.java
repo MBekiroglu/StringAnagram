b4ee7f41d506ccb2400dc64d105105a7846a956b
Mehmet Bekiroglu <mehmet.bekiroglu@outlook.com>
2020-02-10 11:44:53 +0000

    Add files via upload

--- StringAnagram/src/main/java/com/spartaglobal/mb/StringAnagram.java
+++ StringAnagram/src/main/java/com/spartaglobal/mb/StringAnagram.java
@@ -3,12 +3,14 @@ package com.spartaglobal.mb;
 public class StringAnagram {
 
     public boolean isStringAnagram(String string1, String string2) {
+        StringBuilder tester = new StringBuilder(string2);
         int counter = 0;
         for (int i = 0; i < string1.length(); i++) {
             char one = string1.toLowerCase().charAt(i);
-            for (int j = 0; j < string2.length(); j++) {
-                char two = string2.toLowerCase().charAt(j);
+            for (int j = 0; j < tester.length(); j++) {
+                char two = tester.toString().toLowerCase().charAt(j);
                 if (one == two) {
+                    tester.deleteCharAt(j);
                     counter++;
                     break;
                 }
